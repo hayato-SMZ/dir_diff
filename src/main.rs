227a62227a09c5ea433e6900b376c5b4ec73fc17
hayato-SMZ <aegis.tkx@gmail.com>
2023-01-13 14:43:55 +0900

    status output

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,18 @@ struct Args {
 
     #[arg(short, long)]
     target: String,
-    
-    #[arg(short, long, default_value="")]
+
+    #[arg(short, long, default_value = "")]
     out: String,
 }
 fn main() {
     let args = Args::parse();
     let mut source = diff_lib::comparsion_source::ComparsionSource::new();
+    println!("read base path....");
     source.read_base_path(args.source);
+
+    println!("compare ....");
     source.compare_start(args.target.clone());
+    println!("compare end");
     source.result_output(args.out, args.target);
 }
